6a6272ddbb49caf0d0f85529c9970c88848f35c2
林金柱 <jz.lin@quickbird.com>
2013-08-01 21:23:09 +0800

    first commit

---
+++ src/main/java/com/rabbit/pm2d5/core/domain/PM2d5.java
@@ -0,0 +1,11 @@
+package com.rabbit.pm2d5.core.domain;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Mac
+ * Date: 13-7-31
+ * Time: 下午8:59
+ * To change this template use File | Settings | File Templates.
+ */
+public class PM2d5 {
+}
